55ce9913d58bbd08b9d37ca327b6b0e984cb097b
Quy <Quynvhe180901@fpt.edu.vn>
2025-11-15 06:52:51 +0700

    User profile

--- CoffeeShop.Web/Services/IUserApiService.cs
+++ CoffeeShop.Web/Services/IUserApiService.cs
@@ -1,9 +1,13 @@
 ﻿using Shared.Dtos;
+using Shared.Dtos.UserDtos; 
 
 namespace CoffeeShop.Web.Services
 {
     public interface IUserApiService
     {
         Task<BaseResponseDto> GetProfileAsync();
+
+        // Giờ C# đã hiểu "UpdateProfileDto" là gì
+        Task<BaseResponseDto> UpdateProfileAsync(UpdateProfileDto dto);
     }
 }
